ce188577f0e1d5e22761a31d0fcf4de2390e407f
rachtikarn <5910110279@psu.ac.th>
2019-02-18 00:30:13 +0700

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'de-ch'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Bangkok'
 
